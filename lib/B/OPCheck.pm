767055790bae5f62c3df1ecaa416f1be64af7c6f
Yuval Kogman <nothingmuch@woobling.org>
2008-08-04 00:52:48 +0000

    allow unimporting of a certain hook, and in general try to be cleaner about how leavescope is called, esp. on multiple use B::OPCheck imports per scope

--- lib/B/OPCheck.pm
+++ lib/B/OPCheck.pm
@@ -19,18 +19,25 @@ sub import {
 
     $^H |= 0x120000; # set HINT_LOCALIZE_HH + an unused bit to work around a %^H bug
 
-    my $leave_scope = sub {
-        leavescope();
-    };
-
-    my $sg = Scope::Guard->new($leave_scope);
-    $^H{OPCHECK_leavescope} = $sg;
+    my $by_opname = $^H{OPCHECK_leavescope} ||= {};
+    my $guards = $by_opname->{$opname} ||= [];
+    push @$guards, Scope::Guard->new(sub {
+        leavescope($opname, $sub);
+    });
 
     enterscope($opname, $sub );
 }
 
 sub unimport {
-    my ($class) = @_;
+    my ($class, $opname) = @_;
+
+    if ( defined $opname ) { 
+        my $by_opname = $^H{OPCHECK_leavescope};
+        delete $by_opname->{$opname};
+        return if scalar keys %$by_opname; # don't delete other things
+    }
+
+    delete $^H{OPCHECK_leavescope};
     $^H &= ~0x120000;
 }
 
